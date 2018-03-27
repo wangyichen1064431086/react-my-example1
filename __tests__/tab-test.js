8d9727fcbdc86a0520da6ecfc6b042874476d9cc
wangyichen1064431086 <1064431086@qq.com>
2018-03-27 14:56:05 +0800

    Ignore the test folder for karma

--- __tests__/tab-test.js
+++ __tests__/tab-test.js
@@ -1,15 +1,16 @@
-import jest from 'jest';
 
-//jest.unmock('../js/Tabs.js');
+jest.unmock('../js/Tabs');
+jest.unmock('../js/TabPane')
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {ReactTestUtils as TestUtils} from 'react-dom/test-utils';
+import ReactTestUtils from 'react-dom/test-utils';
 import Tabs from '../js/Tabs';
+import TabPane from '../js/TabPane';
 
 describe('Tab', () => {
   it('render the tab content', () => {
-    const tab = TestUtils.renderIntoDocument(
+    const tab = ReactTestUtils.renderIntoDocument(
       <Tabs classPrefix={'tabs'} defaultActiveIndex={0} className="ui-tabs">
         <TabPane order="0" tab="Tab 1">
           第一个Tab里的内容
@@ -24,7 +25,33 @@ describe('Tab', () => {
     );
     const tabNode = ReactDOM.findDOMNode(tab);
 
-    expect(tab.querySelectorAll('.tabs-tab').length).toEqual(3);
-    expect(tab.querySelectorAll('.tabs-tab')[0].classList.contains('tab-active')).toBe(true);
+    expect(tabNode.querySelectorAll('.tabs-tab').length).toEqual(3);
+    expect(tabNode.querySelectorAll('.tabs-tab')[0].classList.contains('tabs-active')).toBe(true);
+  });
+
+  describe('Tab', () => {
+    it('changes active tab after click', () => {
+      const tab = ReactTestUtils.renderIntoDocument(
+        <Tabs classPrefix={'tabs'} defaultActiveIndex={0} className="ui-tabs">
+          <TabPane order="0" tab="Tab 1">
+            第一个Tab里的内容
+          </TabPane>
+          <TabPane order="1" tab="Tab 2">
+            第二个Tab里的内容
+          </TabPane>
+          <TabPane order="2" tab="Tab 3">
+            第三个Tab里的内容
+          </TabPane>
+        </Tabs>
+      );
+      const tabNode = ReactDOM.findDOMNode(tab);
+
+      ReactTestUtils.Simulate.click(
+        tabNode.querySelectorAll('.tabs-tab')[1]
+      );
+
+      expect(tabNode.querySelectorAll('.tabs-tab')[0].classList.contains('tabs-active')).toBe(false);
+      expect(tabNode.querySelectorAll('.tabs-active')[1].classList.contains('tabs-active')).toBe(true);
+    })
   })
 });
